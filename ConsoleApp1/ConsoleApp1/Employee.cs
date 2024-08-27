94a8b29467fe5277157d4d9b7e8195b2e41c2f09
bollajayanth <bollajayanthreddy123@gmail.com>
2024-08-27 21:32:38 +0530

    Fist Commit Contians Delagte and Inheritance

---
+++ ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void PrintName()
+        {
+            Console.WriteLine(this.FirstName + this.LastName);
+        }
+        public void CallDelegateFunction()
+        {
+            Message message = new Message(PrintMessage);
+            message("hello Delegate");
+        }
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class PartTimeEmployee : Employee
+    {
+        public int Salary { get; set; }
+        public string Designation { get; set; }
+        public void PrintSalaryAndDesignation()
+        {
+            Console.WriteLine("Employ Id: " + this.Id + " Salary of the Employee: " + this.Salary + " Designation: " + this.Designation);
+        }
+    }
+    class FullTimeEmployee : Employee
+    {
+        public int Salary { get; set; }
+        public string Designation { get; set; }
+        public void PrintSalaryAndDesignation()
+        {
+            Console.WriteLine("Employ Id: " + this.Id + "Salary of the Employee" + this.Salary + "Designation: " + this.Designation);
+        }
+    }
+
+    public delegate void Message(string message);
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+PartTimeEmployee pte = new PartTimeEmployee();
+pte.FirstName = "Jayanth";
+pte.LastName = "Reddy";
+pte.Salary = 100000;
+pte.Designation = "SDE";
+pte.PrintSalaryAndDesignation();
+pte.CallDelegateFunction();
